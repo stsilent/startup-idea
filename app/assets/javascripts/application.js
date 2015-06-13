a29c9c2256ad872b9d77132484d494d57f661b5b
MonsterCritic <sergeyshmidt-home@Sergeys-iMac.home>
2015-06-13 16:27:09 -0400

    reload on click

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,10 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).mouseup(function (e) {
+    var container = $(".name");
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+         location.reload();
+    }
+});
