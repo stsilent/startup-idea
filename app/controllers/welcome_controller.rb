75fc1ed98bdace8ac0db93e0ed1a36285fb9ad7c
Vladimir Kudinov <pcgamer@mail.ru>
2015-06-14 10:26:33 -0400

    JSON: /welcome/index.json

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,12 @@
 class WelcomeController < ApplicationController
 	def index
 		@titles = Title.order("RANDOM()").limit(2)
+
+   		respond_to do |format|
+		    format.html
+		    format.json { 
+		    	format.json  { render :json => @titles }
+	     	} 
+   		end
 	end
 end
